e73cce74ef9ed9ec8072a442f715876a0d4fd812
Emily Louk <elouk001@odu.edu>
2023-03-13 22:31:44 -0400

    Started unit test for Semester Dir inputs

---
+++ app/src/test/java/edu/odu/cs/cs350/pne/TestSemester.java
@@ -0,0 +1,36 @@
+/*
+ * Unit testing for the Semester class 
+ */
+package edu.odu.cs.cs350.pne;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+class TestSemester {
+    @Test
+    void inputHistoricalEnrDirectory() {
+        // Tests that the correct Semester objs are created for each
+        // historical enrollment directory provided.
+
+        // List of directories will be read into an ArrayList.
+        ArrayList HistoricalEnrDirectories = new ArrayList();
+        HistoricalEnrDirectories.add("201710/");
+        HistoricalEnrDirectories.add("201810/");
+
+        // new Semester obj should be created for each semester directory in the list.
+        // whether a driver or the Semester class itself should do this isn't known yet.
+        for (int i = 0; i < 5; i++) {
+
+            // Semester should have a constructor that takes a directory as parameter
+            Semester s = new Semester(HistoricalEnrDirectories.get(i));
+            assertNotNull(s.getSemesterCode(), "semester should have a code");
+        }
+
+        assertThat(s1.getSemesterCode(), equals("201710"));
+
+    }
+
+    // Add more tests regarding the Semester class below..
+}
