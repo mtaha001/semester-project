44e3c385e1b71623d76ba033699284623eda51fd
Emily Louk <elouk001@odu.edu>
2023-03-14 17:13:23 -0400

    Production code- Semester constructor+attributes

--- app/src/main/java/edu/odu/cs/cs350/pne/Semester.java
+++ app/src/main/java/edu/odu/cs/cs350/pne/Semester.java
@@ -5,8 +5,28 @@
  */
 package edu.odu.cs.cs350.pne;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Semester {
 
+    private String directoryLocation;
+    private String code;
+    private Date preRegistrationDate;
+    private Date addDeadlineDate;
+    private ArrayList<Snapshot> snapshots;
+    private ArrayList<Offering> offerings;
+
+    /**
+     * Construct a Semester object
+     * 
+     * @param directoryLoc - String representing the directory
+     *                     containing the semester's data
+     * @post condition - Semester object is created and has a directoryLocation,
+     *       a semester code, List of Snapshots, preReg date, addDeadline date,
+     *       and a List of Offerings.
+     */
+    public Semester(String directoryLoc) {
+
+    }
 }
