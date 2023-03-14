790b7963771723310c0f74395a412dafe6f8480d
Emily Louk <elouk001@odu.edu>
2023-03-13 20:38:10 -0400

    Created Semester and Snapshot classes

---
+++ app/src/main/java/edu/odu/cs/cs350/pne/Semester.java
@@ -0,0 +1,12 @@
+/*
+ * Semester class, as written in the UML diagram
+ * A semester has multiple Snapshots and multiple Offerings.
+ * A DetailedProjReport and SummaryProjReport depend on a Semester.
+ */
+package edu.odu.cs.cs350.pne;
+
+import java.util.Date;
+
+public class Semester {
+
+}
