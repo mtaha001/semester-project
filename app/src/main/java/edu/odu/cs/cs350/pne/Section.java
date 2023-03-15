5d2971c31a0e31d16a08fd0ac76164a6114d2b92
tuanng2210 <103005131+tuanng2210@users.noreply.github.com>
2023-03-15 00:11:49 -0400

    testing the method to load in csv file

--- app/src/main/java/edu/odu/cs/cs350/pne/Section.java
+++ app/src/main/java/edu/odu/cs/cs350/pne/Section.java
@@ -1,5 +1,17 @@
 package edu.odu.cs.cs350.pne;
 
+import com.opencsv.bean.CsvBindByPosition;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvBindByName;
+
+
+
 public class Section {
+
+    @CsvBindByName(column = "CRN")
+    private String CRN;
+
+    @CsvBindByName(column = "SUBJ")
+    private String SUBJ;
     
 }
