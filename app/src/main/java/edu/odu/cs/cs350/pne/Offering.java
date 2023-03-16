fc8cdc6c87ad7c1f9bf661068153120b2fc5abf4
Boss <mtaha001@odu.edu>
2023-03-15 22:19:06 -0400

    added offering.java

--- app/src/main/java/edu/odu/cs/cs350/pne/Offering.java
+++ app/src/main/java/edu/odu/cs/cs350/pne/Offering.java
@@ -1,5 +1,23 @@
 package edu.odu.cs.cs350.pne;
 
+import com.opencsv.bean.CsvBindByPosition;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvBindByName;
+
+
+
 public class Offering {
+
+    @CsvBindByName(column = "INSTRUCTOR")
+    private String INSTRUCTOR;
+
+    @CsvBindByName(column = "CRSE")
+    private String CRSE; //course
+    
+    @CsvBindByName(column = "OVERALL ENR")
+    private String OVERALLENR; //overall enrollment
+
+    @CsvBindByName(column = "OVERALL CAP")
+    private String OVERALLCAP; //overall capacity
 
 }
