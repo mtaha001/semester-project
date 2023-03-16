eaabf046ee62bcdc8c5e41d8ac855e8236ef98c1
Boss <mtaha001@odu.edu>
2023-03-15 22:10:01 -0400

    unit test for section created

---
+++ app/src/test/java/edu/odu/cs/cs350/pne/TestSection.java
@@ -0,0 +1,36 @@
+/*
+ * Unit testing to check section
+ */
+package edu.odu.cs.cs350.pne;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+public class TestSection {
+
+    private Section section;
+    
+    @Before
+    public void setUp() {
+        section = new Section();
+    }
+    
+    @Test
+    public void testGetSetCRN() {
+        String crn = "12345";
+        section.setCRN(crn);
+        assertEquals(crn, section.getCRN());
+    }
+    
+    @Test
+    public void testGetSetSUBJ() {
+        String subj = "CS";
+        section.setSUBJ(subj);
+        assertEquals(subj, section.getSUBJ());
+    }
+    
+}
+
+
