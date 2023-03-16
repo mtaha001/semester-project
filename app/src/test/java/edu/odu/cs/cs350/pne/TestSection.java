3a22cbc3b39d1a024c1fe10af6f7207ec1891f82
tuannguyen <tnguy039@odu.edu>
2023-03-16 09:50:07 -0400

    add library

--- app/src/test/java/edu/odu/cs/cs350/pne/TestSection.java
+++ app/src/test/java/edu/odu/cs/cs350/pne/TestSection.java
@@ -5,6 +5,7 @@ package edu.odu.cs.cs350.pne;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 import java.util.ArrayList;
 
