f38941d9ed5fd882eca7ae6acc1d5da3c731ef27
Boss <mtaha001@odu.edu>
2023-03-16 21:03:43 -0400

    fixes to section class

--- app/src/main/java/edu/odu/cs/cs350/pne/Section.java
+++ app/src/main/java/edu/odu/cs/cs350/pne/Section.java
@@ -1,18 +1,43 @@
 package edu.odu.cs.cs350.pne;
 
-import com.opencsv.bean.CsvBindByPosition;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.CsvBindByName;
-
-
+// import com.opencsv.bean.CsvBindByPosition;
+// import com.opencsv.bean.CsvToBean;
+// import com.opencsv.bean.CsvBindByName;
 
 public class Section {
 
-    @CsvBindByName(column = "CRN")
-    private String CRN;
-
-    @CsvBindByName(column = "SUBJ")
+    private int CRN;
     private String SUBJ;
+    private String CRSE;
+
     
+    /**
+     * getSUBJ()
+     * 
+     * @return the subject
+     */
+    public String getSUBJ() {
+        return SUBJ;
+    }
+
+    /**
+     * getCRN()
+     * 
+     * @return the CRN
+     */
+    public int getCRN() {
+        return CRN;
+    }
+
+    /**
+     * getCRSE()
+     * 
+     * @return the course
+     */
+    public String getCRSE() {
+        return CRSE;
+    }
+
+
 }
 
