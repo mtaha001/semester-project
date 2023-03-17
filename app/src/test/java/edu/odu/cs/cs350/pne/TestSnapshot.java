662848ac17815d6a2e25f085b9a6a7a85fc569c6
tuannguyen <tnguy039@odu.edu>
2023-03-16 22:27:19 -0400

    fix test for snapshot class

--- app/src/test/java/edu/odu/cs/cs350/pne/TestSnapshot.java
+++ app/src/test/java/edu/odu/cs/cs350/pne/TestSnapshot.java
@@ -7,16 +7,19 @@ package edu.odu.cs.cs350.pne;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+// import org.junit.jupiter.api.BeforeEach;
 
 import java.util.ArrayList;
 
 public class TestSnapshot {
     
 
+    private Snapshot s1; 
+
     @BeforeEach
-    public void setUp(){
-        Snapshot s1 =  new Snapshot();
-        Snapshot s2 =  new Snapshot();
+    public void setUp (){
+        s1 =  new Snapshot();
+        //Snapshot s2 =  new Snapshot();
     }
 
     @Test
