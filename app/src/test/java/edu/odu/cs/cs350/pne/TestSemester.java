a6081db59f26ae03fcb00c5306ff7a7f83206e4c
Emily Louk <elouk001@odu.edu>
2023-03-18 20:05:47 -0400

    edited other Semester tests to pull from test data

--- app/src/test/java/edu/odu/cs/cs350/pne/TestSemester.java
+++ app/src/test/java/edu/odu/cs/cs350/pne/TestSemester.java
@@ -17,8 +17,10 @@ class TestSemester {
 
         // List of directories will be read into an ArrayList.
         ArrayList<String> HistoricalEnrDirectories = new ArrayList<String>();
-        HistoricalEnrDirectories.add("201710/");
-        HistoricalEnrDirectories.add("201810/");
+        HistoricalEnrDirectories.add(
+                "C:\\Users\\izsnu\\OneDrive\\Documents\\ODU\\CS_350\\ENROLLMENTPROJECT\\semester-project\\app\\src\\test\\data\\202010");
+        HistoricalEnrDirectories.add(
+                "C:\\Users\\izsnu\\OneDrive\\Documents\\ODU\\CS_350\\ENROLLMENTPROJECT\\semester-project\\app\\src\\test\\data\\202120");
 
         // new Semester obj should be created for each semester directory in the list.
         // whether a driver or the Semester class itself should do this isn't known yet.
@@ -26,15 +28,17 @@ class TestSemester {
         // Semester should have a constructor that takes a directory as parameter
         Semester s1 = new Semester(HistoricalEnrDirectories.get(0));
         assertNotNull(s1.getDirLoc(), "semester should have a directory location");
-        assertEquals(s1.getDirLoc(), "201710/");
+        assertEquals(s1.getDirLoc(),
+                "C:\\Users\\izsnu\\OneDrive\\Documents\\ODU\\CS_350\\ENROLLMENTPROJECT\\semester-project\\app\\src\\test\\data\\202010");
         assertNotNull(s1.getCode(), "semester should have a code");
-        assertEquals(s1.getCode(), "201710");
+        assertEquals(s1.getCode(), "202010");
 
         Semester s2 = new Semester(HistoricalEnrDirectories.get(1));
         assertNotNull(s2.getDirLoc(), "semester should have a directory location");
-        assertEquals(s2.getDirLoc(), "201810/");
+        assertEquals(s2.getDirLoc(),
+                "C:\\Users\\izsnu\\OneDrive\\Documents\\ODU\\CS_350\\ENROLLMENTPROJECT\\semester-project\\app\\src\\test\\data\\202120");
         assertNotNull(s2.getCode(), "semester should have a code");
-        assertEquals(s2.getCode(), "201810");
+        assertEquals(s2.getCode(), "202120");
 
     }
 
@@ -42,27 +46,27 @@ class TestSemester {
     void yieldsCorrectYearAndSeason() throws IOException {
         ArrayList<String> HistoricalEnrDirectories = new ArrayList<String>();
         HistoricalEnrDirectories.add(
-                "C:\\Users\\izsnu\\OneDrive\\Documents\\ODU\\CS_350\\ENROLLMENTPROJECT\\semester-project\\app\\src\\test\\data\\201710");
+                "C:\\Users\\izsnu\\OneDrive\\Documents\\ODU\\CS_350\\ENROLLMENTPROJECT\\semester-project\\app\\src\\test\\data\\202010");
         HistoricalEnrDirectories.add(
-                "C:\\Users\\izsnu\\OneDrive\\Documents\\ODU\\CS_350\\ENROLLMENTPROJECT\\semester-project\\app\\src\\test\\data\\201820");
+                "C:\\Users\\izsnu\\OneDrive\\Documents\\ODU\\CS_350\\ENROLLMENTPROJECT\\semester-project\\app\\src\\test\\data\\202120");
         HistoricalEnrDirectories.add(
-                "C:\\Users\\izsnu\\OneDrive\\Documents\\ODU\\CS_350\\ENROLLMENTPROJECT\\semester-project\\app\\src\\test\\data\\201930");
+                "C:\\Users\\izsnu\\OneDrive\\Documents\\ODU\\CS_350\\ENROLLMENTPROJECT\\semester-project\\app\\src\\test\\data\\202230");
 
         Semester s1 = new Semester(HistoricalEnrDirectories.get(0));
         assertNotNull(s1.getYear(), "semester should have an associated year");
-        assertEquals(s1.getYear(), 2017);
+        assertEquals(s1.getYear(), 2020);
         assertNotNull(s1.getSeason(), "semester should have an associated season");
         assertEquals(s1.getSeason(), "Fall");
 
         Semester s2 = new Semester(HistoricalEnrDirectories.get(1));
         assertNotNull(s2.getYear(), "semester should have an associated year");
-        assertEquals(s2.getYear(), 2018);
+        assertEquals(s2.getYear(), 2021);
         assertNotNull(s2.getSeason(), "semester should have an associated season");
         assertEquals(s2.getSeason(), "Spring");
 
         Semester s3 = new Semester(HistoricalEnrDirectories.get(2));
         assertNotNull(s3.getYear(), "semester should have an associated year");
-        assertEquals(s3.getYear(), 2019);
+        assertEquals(s3.getYear(), 2022);
         assertNotNull(s3.getSeason(), "semester should have an associated season");
         assertEquals(s3.getSeason(), "Summer");
 
