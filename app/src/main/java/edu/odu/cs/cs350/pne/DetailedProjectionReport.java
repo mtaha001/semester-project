7d235ba67140145cbc4d0f51a71f5adb93551161
tuannguyen <tnguy039@odu.edu>
2023-04-05 15:01:46 -0400

    add tests for DPR class to generate workbook

--- app/src/main/java/edu/odu/cs/cs350/pne/DetailedProjectionReport.java
+++ app/src/main/java/edu/odu/cs/cs350/pne/DetailedProjectionReport.java
@@ -24,6 +24,7 @@ public class DetailedProjectionReport {
          
     }
     
+    
 
     /**
      * getFilePath()
--- app/src/test/java/edu/odu/cs/cs350/pne/TestDetailedProjectionReport.java
+++ app/src/test/java/edu/odu/cs/cs350/pne/TestDetailedProjectionReport.java
@@ -7,6 +7,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.Console;
+import java.io.IOException;
 
 
 import org.junit.jupiter.api.BeforeEach;
@@ -41,6 +43,29 @@ public class TestDetailedProjectionReport {
         assertEquals(DetailedReport1.getFilePath(),"");
         DetailedReport1.setFilePath(outPath);
         assertEquals(DetailedReport1.getFilePath(),"C:/tnguy039/OutPut");
+    }
+
+    @Test
+    public void testCreateWorkbook() throws Exception {
+        String filePath = "C:/path/to/test_workbook.xlsx";
+        
+        // Create a new Excel workbook
+        Workbook workbook = new XSSFWorkbook();
+        
+        // Write the workbook to a file
+        FileOutputStream outputStream = new FileOutputStream(new File(filePath));
+        workbook.write(outputStream);
+        outputStream.close();
+                
+        // Check that the file was created
+        File file = new File(filePath);
+        assertTrue(file.exists());
+        
+        // Check that the file is not empty
+        assertTrue(file.length() > 0);
+        
+        // Delete the file
+        file.delete();
     }
 
    
