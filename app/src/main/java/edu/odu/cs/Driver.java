db1fc6e5f266dab9e9a22f3fb95eb4f145608726
Emily Louk <elouk001@odu.edu>
2023-04-05 18:50:38 -0400

    Driver code

--- app/src/main/java/edu/odu/cs/Driver.java
+++ app/src/main/java/edu/odu/cs/Driver.java
@@ -1,13 +1,44 @@
 package edu.odu.cs;
 
+import java.util.Arrays;
 import edu.odu.cs.cs350.pne.Semester;
 
 public class Driver {
     public static void main(String[] args) {
+
+        validateParameters(args);
+
+        for (int i = 0; i < args.length; i++) {
+
+            if (isSemesterDirectory(args[i].toString()) == true) {
+
+            }
+
+        }
+    }
+
+    private static void validateParameters(String[] args) {
         if (args.length < 3) {
-            System.err.println("Missing arguments");
+            System.err.println("Missing arguments. Please provide the following: \n" +
+                    "1. One or more semester directory locations of historical data.\n" +
+                    "2. A single semester directory location.\n" +
+                    "3. A path for the DPR to be written.\n" +
+                    "4. (Optional): A date as YYYY-MM-DD. \n");
             return;
         }
+    }
+
+    /*
+     * Computes whether or not a command argument is
+     * a Semester directory.
+     * 
+     * @return true if the argument is a Semester Directory
+     * 
+     * @return false if the argument is not a Semester Directory
+     */
+    static boolean isSemesterDirectory(String argument) {
+
+        return true;
     }
 
     /*
@@ -22,8 +53,6 @@ public class Driver {
      * @return false if the argument is not
      */
     boolean isHistoricalSemester() {
-        boolean flag = true;
-
-        return flag;
+        return true;
     }
 }
