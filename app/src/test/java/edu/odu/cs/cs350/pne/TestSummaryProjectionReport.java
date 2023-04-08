b7b84c37092de4f660ce1218f44c59583840fcb5
Moe <mtaha001@odu.edu>
2023-04-07 20:39:06 -0400

    creation of test file for spr

---
+++ app/src/test/java/edu/odu/cs/cs350/pne/TestSummaryProjectionReport.java
@@ -0,0 +1,40 @@
+package edu.odu.cs.cs350.pne;
+
+//unit testing for summary projection report
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.Console;
+import java.io.IOException;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class TestSummaryProjectionReport {
+
+    //@Test
+}
+
+
+    
+
+
+}
+
+
+    
+    
+
+   
+
+
+    
+
