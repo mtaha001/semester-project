1f6fe680c1bd16761c86c86b7b3974899ed9bec7
Moe <mtaha001@odu.edu>
2023-04-07 20:30:33 -0400

    comments / defs for dates

--- app/src/main/java/edu/odu/cs/cs350/pne/SummaryProjectionReport.java
+++ app/src/main/java/edu/odu/cs/cs350/pne/SummaryProjectionReport.java
@@ -6,7 +6,11 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class SummaryProjectionReport {
-    public static double calcPercentElapsed(LocalDate startDate, LocalDate endDate) {
+    public static double calcPercentElapsed(LocalDate startDate, LocalDate endDate, LocalDate snapDate) {
+        //startDate is the beginning of the enrollment period
+        //endDate is the end of the enrollment period
+        //snapDate is the day of the snapshot
+
         double percentElapsed = 0; //placeholder
         return percentElapsed;
     }
