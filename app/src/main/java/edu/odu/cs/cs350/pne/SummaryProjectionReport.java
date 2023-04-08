c033640378f516536064034bdb02c0e7a95d1c99
Moe <mtaha001@odu.edu>
2023-04-07 20:28:04 -0400

    start of percentage calculations

--- app/src/main/java/edu/odu/cs/cs350/pne/SummaryProjectionReport.java
+++ app/src/main/java/edu/odu/cs/cs350/pne/SummaryProjectionReport.java
@@ -2,6 +2,13 @@
 
 package edu.odu.cs.cs350.pne;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
 public class SummaryProjectionReport {
+    public static double calcPercentElapsed(LocalDate startDate, LocalDate endDate) {
+        double percentElapsed = 0; //placeholder
+        return percentElapsed;
+    }
 
 }
