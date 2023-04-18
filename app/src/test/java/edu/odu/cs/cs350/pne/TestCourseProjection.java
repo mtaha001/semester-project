ae5519883bb1db96a0c9806874d061cb3a990086
tuannguyen <tnguy039@odu.edu>
2023-04-18 14:31:27 -0400

    Implment production code for CourseProjection

--- app/src/test/java/edu/odu/cs/cs350/pne/TestCourseProjection.java
+++ app/src/test/java/edu/odu/cs/cs350/pne/TestCourseProjection.java
@@ -1,5 +1,9 @@
 package edu.odu.cs.cs350.pne;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class TestCourseProjection {
     @Test
     public void testGetName() {
