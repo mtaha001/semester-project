2ae8e3e3f0b43c06c19a393e42563fc28976b510
Emily Louk <elouk001@odu.edu>
2023-04-24 21:34:45 -0400

    phase5

--- app/src/main/java/edu/odu/cs/cs350/pne/Driver.java
+++ app/src/main/java/edu/odu/cs/cs350/pne/Driver.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
-/**The Driver for the enrollment projection program */
+/**The Driver for the enrollment projection program. */
 public class Driver {
 
     /**Evaluates the parameters passed and calls on the appropriate
